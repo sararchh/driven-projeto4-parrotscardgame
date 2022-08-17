1ce3fd39dd72d16a843dd95cf4b4c6b892eebda0
Sararchh <sara_rocha99@hotmail.com>
2022-08-16 23:08:45 -0300

    add image

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-function askLetters() {
-  let num = 0;
+let cardsArray = [];
 
-  do {
-    num = prompt("Digite a quantidade de cards para jogar (somente pares)");
-  } while (num % 2 !== 0 || num == 0 || num < 4 || num > 14);
+let imageArray = [
+  "./assets/explodyparrot.gif",
+  "./assets/bobrossparrot.gif",
+  "./assets/fiestaparrot.gif",
+  "./assets/metalparrot.gif",
+  "./assets/revertitparrot.gif",
+  "./assets/tripletsparrot.gif",
+  "./assets/unicornparrot.gif"
+];
 
+let cards = 0;
 
+while (cards % 2 !== 0 || cards == 0 || cards < 4 || cards > 14) {
+  cards = prompt("Digite a quantidade de cartas para jogar novamente: (somente pares)");
+  cardsArray.push(cards);
 }
 
-askLetters();
+const list = document.querySelector('ul');
+
+let indice = 0;
+while (cardsArray[cardsArray.length - 1] > indice) {
+  list.innerHTML = list.innerHTML + `
+  <li class="card-flipper">
+    <div class="front">
+      <img src="./assets/front.png" alt="papagaio verde">
+    </div>
+    <div class="back">
+       <img src=${imageArray[indice]} />
+     </div>
+  </li>
+  `;
+  indice = indice + 1;
+}
